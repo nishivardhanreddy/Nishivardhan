b957a5fafbfea9b374732cd32f1121b392621b35
nishivardhanreddy <nishivardhanreddy99@gmail.com>
2025-07-07 00:54:36 +0530

    updated portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: '/Nishivardhan/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
